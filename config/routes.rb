ae336896e5a4a2fc316774108ee07ff668b3afae
robot <robot65536@gmail.com>
2009-04-30 00:35:48 +0900

    Added UserSession and User models and controllers.
    Not coded view files yet.
    Not tested yet.

--- config/routes.rb
+++ config/routes.rb
@@ -35,6 +35,11 @@ ActionController::Routing::Routes.draw do |map|
 
   # See how all your routes lay out with "rake routes"
 
+  map.resource :account, :controller => "users"
+  map.resources :users
+  map.resource :user_session
+  map.root :controller => "user_session", :action => "new"
+
   # Install the default routes as the lowest priority.
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing the them or commenting them out if you're using named routes and resources.
